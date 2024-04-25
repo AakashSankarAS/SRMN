1b31543c4a9cf8696e2fcb4cee09135a6cf04a7b
Mona Lisa <aakashsankar412@gmail.com>
2024-04-25 15:09:10 +0530

    zoho sign

--- src/nmrs/pages/thankyou/ThankYou.js
+++ src/nmrs/pages/thankyou/ThankYou.js
@@ -1,6 +1,25 @@
-import { Card, CardBody, CardHeader, Container } from "reactstrap";
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Container,
+  Row,
+} from "reactstrap";
 
 export default function ThankYou() {
+  const [sign, setSign] = useState("");
+  const handleClick = async () => {
+    fetch("http://192.168.0.100:4000/embeddedsigning")
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        setSign(response.url);
+      });
+  };
   return (
     <Container>
       <Card>
@@ -9,7 +28,7 @@ export default function ThankYou() {
           <h4>Share your FeedBack</h4>
         </CardHeader>
         <CardBody>
-          <iframe
+          {/* <iframe
             src="https://survey.zohopublic.in/zs/pHB0za"
             frameborder="0"
             style={{ height: "700px", width: "100%" }}
@@ -17,11 +36,19 @@ export default function ThankYou() {
             marginheight="0"
             scrolling="auto"
             allow="geolocation"
-          ></iframe>
+          ></iframe> */}
+          <Row>
+            <Col className="text-center">
+              <Button onClick={handleClick} className="bg-usrb text-white">
+                Zoho Api Hit
+              </Button>
+            </Col>
+          </Row>
           <iframe
-            src="https://sign.zoho.in/zsguest?locale=en&sign_id=234b4d535f495623da4eee598214ff8debbd391c1006531c2ee1c1c2593d0c230ca9ce322fab758c3f49d371573adcea76daa2b9859cf1991e6554a52904c73f2f53e78a054fd8cc29f56c6a164846848ad990de40800e47e38694159be8ec488b7d1aad0df8242f851eacefb9dd4cf6525fd9fe974cbf1b0485525d42cb4e6d1a900fcc027192f4&frameorigin=https://srmn.vercel.app"
+            src={sign}
+            title="NMRS SURVEY"
             frameborder="0"
-            style={{ height: "700px", width: "100%" }}
+            style={{ minHeight: "180vh", width: "100%" }}
             marginwidth="0"
             marginheight="0"
             scrolling="auto"
